7c296f7223f469b99cfa59304bc29af9028965c1
NovWyatt <rirtar.lofi@gmail.com>
2025-05-12 22:10:44 +0700

    fix vercel v7.10

--- next.config.js
+++ next.config.js
@@ -8,14 +8,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   swcMinify: true,
-  output: 'export',
-  // Cấu hình này tạo ra static HTML/CSS/JS thay vì sử dụng Next.js server
-  // Điều này sẽ giúp Vercel hiển thị trang web đúng cách
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  // Bỏ output: 'export' để sử dụng server-side rendering
 }
 
 module.exports = nextConfig
